9ca9e2059983b9d87f71e030d9ac6afe50631cb5
Jzz-C <ygyi8632@gmail.com>
2023-12-01 12:06:37 +0630

    Set up next theme and fix logo import error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/provider/theme-provider";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Tom.dev",
   icons: {
-    icon: ["/favicon-dev.png"],
+    icon: ["/logo-dev.png"],
   },
   description: "This is my portfolio",
 };
@@ -19,7 +25,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import ThemeToggle from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello htet htet</main>;
+  return (
+    <main>
+      Hello htet htet ko chit nay Pi <ThemeToggle />
+    </main>
+  );
 }
